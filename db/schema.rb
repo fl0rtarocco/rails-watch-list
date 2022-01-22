96e1bd8ffba36266a65efc7166566e4b9f766445
Florencia Tarocco <florenciatarocco@gmail.com>
2022-01-22 16:42:51 +0100

    create controllers and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_22_103155) do
+ActiveRecord::Schema.define(version: 2022_01_22_132347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2022_01_22_103155) do
     t.string "title"
     t.text "overview"
     t.string "poster_url"
-    t.decimal "rating"
+    t.float "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
