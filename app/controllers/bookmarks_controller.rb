faacb9a93dc392893c8b8e1912d55f98780c09a2
Florencia Tarocco <florenciatarocco@gmail.com>
2022-01-25 18:43:39 +0100

    clean git status

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -18,7 +18,7 @@ class BookmarksController < ApplicationController
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@bookmark.list)
+    redirect_to list_path(@bookmark.list0)
   end
 
   private
